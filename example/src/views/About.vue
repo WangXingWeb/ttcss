29b5fe053b923645ddac388a852a1140e16ee6c0
星仔 <wx1620@163.com>
2021-12-03 23:05:07 +0800

    加入examppple

---
+++ example/src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="about text-danger box-border mt-5">
+        <h1 class="mt--1 fs-3xl">This is an about page</h1>
+
+        <table style="width: 100%">
+            <thead>
+                <tr>
+                    <th>dd</th>
+                    <th>德德</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+</template>
